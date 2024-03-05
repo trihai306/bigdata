4bb333868240643a216f222b1553690a8db1f611
Trihai <68975387+trihai306@users.noreply.github.com>
2024-03-05 20:17:21 +0700

    u

--- app/Restify/CommentRepository.php
+++ app/Restify/CommentRepository.php
@@ -58,7 +58,7 @@ class CommentRepository extends Repository
     {
         $post = Post::find($resource->post_id);
         $user = User::find($post->user_id);
-        sendFirebaseNotification($user->phone_token, 'bạn có comment', 'Push notification Dina app', ['type' => 'comment', 'id' => $resource->post_id]);
+        sendFirebaseNotification((string)$user->phone_token, 'bạn có comment', 'Push notification Dina app', ['type' => 'comment', 'id' => (string)$resource->post_id]);
         $user->notify(new PostNotification('comment', 'abc', 'có lượt comment',$resource->post_id));
     }
 }
