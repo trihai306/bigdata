271bd5b425905acca450ae135027cdbf5b2a5de0
Trihai <68975387+trihai306@users.noreply.github.com>
2024-03-23 15:45:35 +0700

    u

--- app/Http/Controllers/API/UserConversationController.php
+++ app/Http/Controllers/API/UserConversationController.php
@@ -9,8 +9,8 @@ class UserConversationController extends Controller
 {
     public function updateLastSeenMessage(Request $request, $conversationId)
     {
-        $validatedData = $request->validate([
-            'last_seen_message_id' => 'required|exists:messages,id'
+       $request->validate([
+            'message_id' => 'required|exists:messages,id'
         ]);
         $userConversation = auth()->user()->userConversations()->where('conversation_id', $conversationId)->first();
         $userConversation->update(['last_seen_message_id' => $request->message_id]);
