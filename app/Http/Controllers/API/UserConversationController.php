d1ed7478adba57d322a0da377aa3c6bb3d44c240
Trihai <68975387+trihai306@users.noreply.github.com>
2024-03-23 15:52:28 +0700

    u

--- app/Http/Controllers/API/UserConversationController.php
+++ app/Http/Controllers/API/UserConversationController.php
@@ -9,11 +9,17 @@ class UserConversationController extends Controller
 {
     public function updateLastSeenMessage(Request $request, $conversationId)
     {
-       $request->validate([
+        $request->validate([
             'message_id' => 'required|exists:messages,id'
         ]);
+
         $userConversation = auth()->user()->userConversations()->where('conversation_id', $conversationId)->first();
-        $userConversation->update(['last_seen_message_id' => $request->message_id]);
-        return response()->json(['message' => 'success']);
+
+        if ($userConversation) {
+            $userConversation->update(['last_seen_message_id' => $request->message_id]);
+            return response()->json(['message' => 'success']);
+        } else {
+            return response()->json(['error' => 'No matching user conversation found'], 404);
+        }
     }
 }
