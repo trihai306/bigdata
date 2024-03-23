5ff63101052b4bbbfefa6f5a4ba9f58b738982fa
Trihai <68975387+trihai306@users.noreply.github.com>
2024-03-23 16:16:42 +0700

    u

--- app/Models/UserConversation.php
+++ app/Models/UserConversation.php
@@ -17,6 +17,7 @@ class UserConversation extends Model
 
     // Disable incrementing
     public $incrementing = false;
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -30,5 +31,33 @@ class UserConversation extends Model
     public function lastSeenMessage()
     {
         return $this->belongsTo(Message::class, 'last_seen_message_id');
+    }
+
+    // Override the getKeyForSaveQuery method
+    protected function getKeyForSaveQuery()
+    {
+        $keys = [];
+
+        foreach ($this->getKeyName() as $key) {
+            if (! isset($this->$key)) {
+                throw new Exception(__METHOD__.'Missing part of the primary key: '.$key);
+            }
+
+            $keys[$key] = $this->$key;
+        }
+
+        return $keys;
+    }
+
+    // Override the setKeysForSaveQuery method
+    protected function setKeysForSaveQuery($query)
+    {
+        $keys = $this->getKeyForSaveQuery();
+
+        foreach ($keys as $keyName => $keyValue) {
+            $query->where($keyName, '=', $keyValue);
+        }
+
+        return $query;
     }
 }
