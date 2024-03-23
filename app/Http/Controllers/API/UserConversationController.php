48384ffb42008c20aa6e339047f2263fb830bb72
Trihai <68975387+trihai306@users.noreply.github.com>
2024-03-23 15:59:47 +0700

    u

--- app/Http/Controllers/API/UserConversationController.php
+++ app/Http/Controllers/API/UserConversationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserConversation;
 use Illuminate\Http\Request;
 
 class UserConversationController extends Controller
@@ -13,10 +14,13 @@ class UserConversationController extends Controller
             'message_id' => 'required|exists:messages,id'
         ]);
 
-        $userConversation = auth()->user()->userConversations()->where('conversation_id', $conversationId)->first();
+        $userConversation = UserConversation::where('conversation_id', $conversationId)
+            ->where('user_id', $request->user_id)
+            ->first();
 
         if ($userConversation) {
-            $userConversation->update(['last_seen_message_id' => $request->message_id]);
+            $userConversation->last_seen_message_id = $request->message_id;
+            $userConversation->save();
             return response()->json(['message' => 'success']);
         } else {
             return response()->json(['error' => 'No matching user conversation found'], 404);
