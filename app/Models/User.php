8c52916ae808c5ee627645f1cf44c85fef422e46
Trihai <68975387+trihai306@users.noreply.github.com>
2024-04-04 22:48:13 +0700

    u

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
     ];
 
     protected $hidden = [
-         'remember_token',
+        'password', 'remember_token',
     ];
     protected $dates = ['last_login_at', 'updated_at', 'created_at','birthday'];
 
