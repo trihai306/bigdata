6c118a1dd21db38411d9f02588a03fb5f56d6df8
Trihai <68975387+trihai306@users.noreply.github.com>
2024-04-04 22:19:48 +0700

    u

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -108,8 +108,9 @@ class AuthController extends Controller
             'field' => 'type,seller|string|in:leather_goods,clothing,all',
             'avatar' => 'image|mimes:jpeg,png,jpg,gif,svg',
         ]);
-        //validate xem mật khẩu cũ có đúng không
-        if (Hash::check($request->password, $request->user()->password)) {
+
+        $user = User::find($request->user()->id);
+        if (Hash::check($request->password, $user->password)) {
             return response()->json(['message' => 'Mật khẩu không chính xác'], 400);
         }
         if ($request->has('avatar')) {
