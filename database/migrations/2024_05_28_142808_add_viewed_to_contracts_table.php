321b200c07c774dc6aa76abf10b304ac17912476
Trihai <68975387+trihai306@users.noreply.github.com>
2024-05-28 21:35:05 +0700

    u

--- database/migrations/2024_05_28_142808_add_viewed_to_contracts_table.php
+++ database/migrations/2024_05_28_142808_add_viewed_to_contracts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('contracts', function (Blueprint $table) {
-            $table->bigInteger('post_id')->unique();
+            $table->bigInteger('post_id')->unique()->nullable();
             $table->boolean('viewed')->default(false)->after('status');
         });
     }
