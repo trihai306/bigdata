9a865c087be891bf6906014d9b84eda20419efa5
Trihai <68975387+trihai306@users.noreply.github.com>
2024-07-11 22:18:32 +0700

    u

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,15 +3,19 @@ import Pusher from 'pusher-js';
 import Alpine from 'alpinejs';
 import ClipboardJS from "clipboard";
 import intersect from '@alpinejs/intersect'
+import mask from '@alpinejs/mask'
+import sort from '@alpinejs/sort'
 
+Alpine.plugin(mask)
+Alpine.plugin(sort)
 Alpine.plugin(intersect)
 window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY??'app-key',
-    wsHost:import.meta.env.VITE_PUSHER_HOST?? '127.0.0.1',
-    wsPort: import.meta.env.VITE_PUSHER_PORT??'',
-    wssPort: import.meta.env.VITE_PUSHER_PORT??'',
+    key: import.meta.env.VITE_PUSHER_APP_KEY ?? 'app-key',
+    wsHost: import.meta.env.VITE_PUSHER_HOST ?? '127.0.0.1',
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? '',
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? '',
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
     forceTLS: false,
     encrypted: true,
@@ -22,11 +26,11 @@ window.Echo = new Echo({
 // Instantiate clipboard
 var clipboard = new ClipboardJS('.btn-copy');
 
-clipboard.on('success', function(e) {
+clipboard.on('success', function (e) {
     e.clearSelection();
 });
 
-clipboard.on('error', function(e) {
+clipboard.on('error', function (e) {
 });
 
 async function handleFiles(files) {
