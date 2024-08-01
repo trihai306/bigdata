a187f24d7f959b1e0083379822233c34eba78f19
Trihai <68975387+trihai306@users.noreply.github.com>
2024-08-01 21:22:09 +0700

    u

--- app/Http/Controllers/API/ViettelPostController.php
+++ app/Http/Controllers/API/ViettelPostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Delivery;
 use App\Models\Product;
 use App\Models\User;
+use App\Models\UserDeliveryInfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use ViettelPostAPI;
@@ -18,11 +19,10 @@ class ViettelPostController extends Controller
             // Thu thập thông tin từ request
             $inputDelivery = $request->only(['sender_id', 'receiver_id']);
             $product = $request->only(['weight', 'price', 'length', 'width', 'height']);
-            dd($inputDelivery);
             // Tìm thông tin giao hàng của người gửi và người nhận
-            $sender = Delivery::find($inputDelivery['sender_id']);
-            $receiver = Delivery::find($inputDelivery['receiver_id']);
-            dd($sender, $receiver);
+            $sender = UserDeliveryInfo::find($inputDelivery['sender_id']);
+            $receiver = UserDeliveryInfo::find($inputDelivery['receiver_id']);
+
             // Kiểm tra người gửi và người nhận có tồn tại không
             if (!$sender || !$receiver) {
                 return response()->json(['error' => 'Không tìm thấy thông tin người gửi hoặc người nhận.'], 404);
@@ -61,8 +61,8 @@ class ViettelPostController extends Controller
     {
        $inputDelivery = $request->input(['sender_id', 'receiver_id']);
         $product = $request->input(['weight', 'price', 'length', 'width', 'height']);
-        $sender = Delivery::find($inputDelivery['sender_id']);
-        $receiver = Delivery::find($inputDelivery['receiver_id']);
+        $sender = UserDeliveryInfo::find($inputDelivery['sender_id']);
+        $receiver = UserDeliveryInfo::find($inputDelivery['receiver_id']);
 
         if (!$sender || !$receiver) {
             return response()->json(['error' => 'Không tìm thấy thông tin người gửi hoặc người nhận.'], 204);
