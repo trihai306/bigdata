3106ac234382d95a9e05773f4197a3ffc7e9f953
Trihai <68975387+trihai306@users.noreply.github.com>
2024-08-04 15:44:43 +0700

    u

--- app/Http/Controllers/API/ViettelPostController.php
+++ app/Http/Controllers/API/ViettelPostController.php
@@ -101,48 +101,64 @@ class ViettelPostController extends Controller
 
     public function createOrder(Request $request)
     {
-        $sender = UserDeliveryInfo::find($request->get('sender_id'));
-        $receiver = UserDeliveryInfo::find($request->get('receiver_id'));
-        $product = $request->input(['weight', 'price', 'length', 'width', 'height', 'product_name','note','quantity']);
-        $order = $request->input(['order_servie', 'order_note']);
-        $contract = $request->input(['contract_id']);
-        $listProduct = Product::where('contract_id', $contract)->get();
-        $listItem = [];
-        foreach ($listProduct as $product) {
-            $listItem[] = [
-                "PRODUCT_NAME" => $product->name,
-                "PRODUCT_QUANTITY" => $product->quantity,
-                "PRODUCT_PRICE" => $product->price,
-                'PRODUCT_WEIGHT' => 100,
+        try {
+            $sender = UserDeliveryInfo::find($request->get('sender_id'));
+            $receiver = UserDeliveryInfo::find($request->get('receiver_id'));
+            if (!$sender || !$receiver) {
+                throw new \Exception("Không tìm thấy thông tin người gửi hoặc người nhận.");
+            }
+
+            $product = $request->input(['weight', 'price', 'length', 'width', 'height', 'product_name', 'note', 'quantity']);
+            $order = $request->input(['order_service', 'order_note']);
+            $contract = $request->input('contract_id');
+
+            $listProduct = Product::where('contract_id', $contract)->get();
+            if ($listProduct->isEmpty()) {
+                throw new \Exception("Không tìm thấy sản phẩm nào cho mã hợp đồng đã cung cấp.");
+            }
+
+            $listItem = [];
+            foreach ($listProduct as $product) {
+                $listItem[] = [
+                    "PRODUCT_NAME" => $product->name,
+                    "PRODUCT_QUANTITY" => $product->quantity,
+                    "PRODUCT_PRICE" => $product->price,
+                    'PRODUCT_WEIGHT' => 100,
+                ];
+            }
+
+            $orderDetails = [
+                "SENDER_FULLNAME" => $sender->receiver_name,
+                "SENDER_ADDRESS" => $sender->address,
+                "SENDER_PHONE" => $sender->phone,
+                "RECEIVER_FULLNAME" => $receiver->receiver_name,
+                "RECEIVER_ADDRESS" => $receiver->address,
+                "RECEIVER_PHONE" => $receiver->phone,
+                "PRODUCT_NAME" => $product['product_name'],
+                "PRODUCT_DESCRIPTION" => $product['note'],
+                "PRODUCT_QUANTITY" => $product['quantity'],
+                "PRODUCT_PRICE" => $product['price'],
+                "PRODUCT_WEIGHT" => $product['weight'],
+                "PRODUCT_LENGTH" => $product['length'],
+                "PRODUCT_WIDTH" => $product['width'],
+                "PRODUCT_HEIGHT" => $product['height'],
+                "ORDER_PAYMENT" => 3,
+                "ORDER_SERVICE" => $order['order_service'],
+                "PRODUCT_TYPE" => "HH",
+                "ORDER_SERVICE_ADD" => null,
+                "ORDER_NOTE" => $order['order_note'],
+                "MONEY_COLLECTION" => 56827,
+                "EXTRA_MONEY" => 0,
+                "CHECK_UNIQUE" => true,
+                "LIST_ITEM" => $listItem
             ];
+
+            $ViettelPostAPI = new ViettelPostAPI();
+            $response = $ViettelPostAPI->createOrder($orderDetails);
+            return response()->json($response);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
         }
-        $orderDetails = [
-            "SENDER_FULLNAME" => $sender->receiver_name,
-            "SENDER_ADDRESS" => $sender->address,
-            "SENDER_PHONE" => $sender->phone,
-            "RECEIVER_FULLNAME" => $receiver->receiver_name,
-            "RECEIVER_ADDRESS" => $receiver->address,
-            "RECEIVER_PHONE" => $receiver->phone,
-            "PRODUCT_NAME" => $product['product_name'],
-            "PRODUCT_DESCRIPTION" => $product['note'],
-            "PRODUCT_QUANTITY" => $product['quantity'],
-            "PRODUCT_PRICE" => $product['price'],
-            "PRODUCT_WEIGHT" => $product['weight'],
-            "PRODUCT_LENGTH" => $product['length'],
-            "PRODUCT_WIDTH" => $product['width'],
-            "PRODUCT_HEIGHT" => $product['height'],
-            "ORDER_PAYMENT" => 3,
-            "ORDER_SERVICE" => $order['order_service'],
-            "PRODUCT_TYPE" => "HH",
-            "ORDER_SERVICE_ADD" => null,
-            "ORDER_NOTE" => $order['order_note'],
-            "MONEY_COLLECTION" => 56827,
-            "EXTRA_MONEY" => 0,
-            "CHECK_UNIQUE" => true,
-            "LIST_ITEM" => $listItem
-        ];
-        $ViettelPostAPI = new ViettelPostAPI();
-        $response = $ViettelPostAPI->createOrder($orderDetails);
-        return response()->json($response);
     }
+
 }
